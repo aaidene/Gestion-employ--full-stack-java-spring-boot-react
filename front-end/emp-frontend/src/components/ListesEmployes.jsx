ba9284d759e2b4ff315f5aa6d7d560e42a3c7547
aaidene <aidene.abdelhakim@hotmail.fr>
2024-03-18 23:49:10 +0100

    récureparation api getAllemployes reussi

--- front-end/emp-frontend/src/components/ListesEmployes.jsx
+++ front-end/emp-frontend/src/components/ListesEmployes.jsx
@@ -1,69 +1,44 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { listesEmployes } from '../services/EmployeService';
 
 function ListesEmployes() {
-   const data = [
-  {
-    id: 1,
-    nom: "David",
-    prenom: "Marque",
-    email: "test@gmail.com"
-  },
-  {
-    id: 2,
-    nom: "Alice",
-    prenom: "Johnson",
-    email: "alice.johnson@example.com"
-  },
-  {
-    id: 3,
-    nom: "John",
-    prenom: "Doe",
-    email: "john.doe@example.com"
-  },
-  {
-    id: 4,
-    nom: "Emma",
-    prenom: "Smith",
-    email: "emma.smith@example.com"
-  }
-];
+    const [employes, setEmployes] = useState([]);
 
+    useEffect(() => {
+        listesEmployes().then((response) => {
+            setEmployes(response.data);
+        }).catch(error => {
+            console.error(error);
+        });
+    });
 
-  return (
-    
-    <div className='container'>
+   
+
+    return (
+        <div className='container'>
             <h2 className='text-center'>Listes des employés</h2>
-
-        <table className='table table-striped table-bordered'>
-  <thead>
-    <tr >
-      <th >Employe Id</th>
-      <th >Nom</th>
-      <th >Prenom</th>
-      <th>email</th>
-    </tr>
-  </thead>
-  <tbody>
-
-    {data.map(employe=>
-      <tr key={employe.id}>
-        <td>{employe.id}</td>
-       <td>{employe.nom}</td>
-           <td>{employe.prenom}</td>
-               <td>{employe.email}</td>
-    </tr>
-    )}
-
-  
-  
-  </tbody>
-</table>
-
-
-     </div>
-
-  )
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Identifiant Employé</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employes.map(employe =>
+                        <tr key={employe.id}>
+                            <td>{employe.id}</td>
+                            <td>{employe.nom}</td>
+                            <td>{employe.prenom}</td>
+                            <td>{employe.email}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default ListesEmployes
+export default ListesEmployes;
